f086fe674e115fa431191b588b1bbf4b0ef78a28
Francisco José García García <frangarcj@gmail.com>
2021-01-19 11:19:12 +0100

    actions: create dependency json

---
+++ create-matrix.js
@@ -0,0 +1,93 @@
+const testFolder = '.';
+const fs = require('fs');
+const path = require('path')
+
+const dependant = new Map();
+
+const args = process.argv.slice(2);
+
+let isDependant = false;
+
+switch (args[0]) {
+    case 'dependant':
+        isDependant = true;
+        break;
+    case 'non_dependant':
+        break;
+    default:
+        return;
+}
+
+resolveDependency = (name, map) => {
+    const dependencies = map.get(name);
+    if (!dependencies) {
+        return [];
+    }
+
+    dependencies.forEach((dependency) => {
+        const deps = resolveDependency(dependency, map);
+        if (deps.length > 0) {
+            deps.forEach((dep) => {
+                dependencies.push(dep);
+            });
+        }
+    });
+
+    return dependencies;
+
+}
+
+
+let directories = fs.readdirSync(testFolder).filter(file => fs.lstatSync(file).isDirectory());
+
+directories = directories.filter(directory => {
+    return fs.readdirSync(directory).filter(file => file == 'VITABUILD').length > 0;
+});
+
+directories.forEach(directory => {
+    const content = fs.readFileSync(path.join(directory, "VITABUILD"));
+    const lines = content.toString().split('\n');
+    for (let i = 0; i < lines.length; i++) {
+        const KV = lines[i].split("=");
+        if (KV.length > 1 && (KV[0].trim() == 'depends' || KV[0].trim() == 'makedepends')) {
+
+            const dependencies = KV[1].trim().replace("(", "").replace(")", "").replace(/\'||\)/g, "").split(' ');
+            dependant.set(directory, dependencies);
+
+            break;
+        }
+    }
+
+})
+
+
+dependant.forEach((value, key) => {
+    resolveDependency(key, dependant);
+})
+
+const output = {
+    non_dependant: [],
+    dependant: []
+};
+
+directories.forEach(library => {
+    const dependencies = dependant.get(library);
+    if (dependencies) {
+        let info = library;
+        const deps = [];
+        dependencies.forEach((dependency) => {
+            if (dependant.get(dependency))
+                deps.push(dependency);
+        });
+        deps.forEach(dependency => info = dependency + " " + info);
+        output.dependant.push(info);
+    } else {
+        output.non_dependant.push(library);
+    }
+})
+
+if(isDependant)
+    console.log(JSON.stringify(output.dependant))
+else
+    console.log(JSON.stringify(output.non_dependant))
+
