7e266595893d1e70cbd8a53acc221ee6ff6d031a
scribam <scribam@users.noreply.github.com>
2026-01-02 09:09:53 +0100

    build: shallow clone vdpm and vita-makepkg

--- prepare.sh
+++ prepare.sh
@@ -7,13 +7,13 @@ echo "|1|K7QZQZJexBCVdpiZ9eTIRbn7ZyA=|uP3PBfEWXFeesWyVtr8kdAbYGsM= ssh-rsa AAAAB
 echo "|1|XV846KwSDydQe2vdRFgX4bci360=|G5NzDWkFsXhXo3Koq8j+1vOl8WA= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBF22ib7zM5u9ApfbW/xUg8unpnr1LuTTCvPf4MdmLFhEmqxrSrnt0Q7x/wN/Vo3w/RxjmiK/ukjGSWulp/PWBKE=" >> ~/.ssh/known_hosts
 echo "|1|TFiAy+cxsZWXgU3EDeOtikdtDbY=|EyVMn+ixhtL8cq3ZrZWYGbe/vO8= ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWdwNPZ309h8vxSAk8Ruq5VkpZdWauQqjYW5KAepqnm" >> ~/.ssh/known_hosts
 pushd ..
-git clone https://github.com/vitasdk/vdpm.git
+git clone --depth=1 https://github.com/vitasdk/vdpm.git
 cd vdpm
 bash bootstrap-vitasdk.sh
 popd
 
 pushd ..
-git clone https://github.com/vitasdk/vita-makepkg.git
+git clone --depth=1 https://github.com/vitasdk/vita-makepkg.git
 cd vita-makepkg
 cp makepkg.conf.sample makepkg.conf
 popd
