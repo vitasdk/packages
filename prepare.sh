afe6cb1385ebdadadaedcf8d1a2ca3e5c72abc06
scribam <scribam@users.noreply.github.com>
2026-01-04 11:24:08 +0100

    build: use vita-makepkg from vitasdk

--- prepare.sh
+++ prepare.sh
@@ -8,11 +8,5 @@ cd vdpm
 bash bootstrap-vitasdk.sh
 popd
 
-pushd ..
-git clone --depth=1 https://github.com/vitasdk/vita-makepkg.git
-cd vita-makepkg
-cp makepkg.conf.sample makepkg.conf
-popd
-
 export VITASDK=/usr/local/vitasdk
-export PATH=$(pwd)/../vita-makepkg:$VITASDK/bin:$PATH
+export PATH=$VITASDK/bin:$PATH
